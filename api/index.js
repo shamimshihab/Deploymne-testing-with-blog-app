fd74102fb4d892b989e817724b35ed8b87623c16
Shamim Shihab <shamimshihab56@gmail.com>
2023-11-15 17:39:34 -0800

    another try

--- api/index.js
+++ api/index.js
@@ -111,35 +111,36 @@ app.get("/aboutMe", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
-  let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-  }
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { id, title, summary, content } = req.body;
-    const postDoc = await Post.findById(id);
-    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-    if (!isAuthor) {
-      return res.status(400).json("you are not the author");
-    }
-    await postDoc.update({
-      title,
-      summary,
-      content,
-      cover: newPath ? newPath : postDoc.cover,
-    });
+// app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+//   let newPath = null;
+//   if (req.file) {
+//     const { originalname, path } = req.file;
+//     const parts = originalname.split(".");
+//     const ext = parts[parts.length - 1];
+//     newPath = path + "." + ext;
+//     fs.renameSync(path, newPath);
+//   }
 
-    res.json(postDoc);
-  });
-});
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { id, title, summary, content } = req.body;
+//     const postDoc = await Post.findById(id);
+//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+//     if (!isAuthor) {
+//       return res.status(400).json("you are not the author");
+//     }
+//     await postDoc.update({
+//       title,
+//       summary,
+//       content,
+//       cover: newPath ? newPath : postDoc.cover,
+//     });
+
+//     res.json(postDoc);
+//   });
+// });
 
 app.delete("/post/:id", async (req, res) => {
   const postId = req.params.id;
