9895471fe8ae2590091cd9a42651588e71fc051c
Shamim Shihab <shamimshihab56@gmail.com>
2023-11-19 20:10:17 -0800

    file upload bug fix

--- api/index.js
+++ api/index.js
@@ -9,12 +9,21 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
-const uploadMiddleware = multer({ dest: "uploads/" });
+
 const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 mongoose.set("strictQuery", false);
+
+// image file rendering prob testing
+// const uploadMiddleware = multer({ dest: "uploads/" });
+
+// Define an absolute path for uploads
+const uploadPath = path.join(__dirname, "uploads/");
+app.use("/uploads", express.static(uploadPath));
+// Configure multer to use the absolute upload path
+const uploadMiddleware = multer({ dest: uploadPath });
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // app.use(
@@ -163,11 +172,63 @@ app.post("/logout", (req, res) => {
 //   });
 // });
 
+// app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+//   const { originalname, path } = req.file;
+//   const parts = originalname.split(".");
+//   const ext = parts[parts.length - 1];
+//   const newPath = path + "." + ext;
+//   fs.renameSync(path, newPath);
+
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { title, summary, content } = req.body;
+//     const postDoc = await Post.create({
+//       title,
+//       summary,
+//       content,
+//       cover: newPath,
+//       author: info.id,
+//     });
+//     res.json(postDoc);
+//   });
+// });
+
+// app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+//   let newPath = null;
+//   if (req.file) {
+//     const { originalname, path } = req.file;
+//     const parts = originalname.split(".");
+//     const ext = parts[parts.length - 1];
+//     newPath = path + "." + ext;
+//     fs.renameSync(path, newPath);
+//   }
+
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { id, title, summary, content } = req.body;
+//     const postDoc = await Post.findById(id);
+//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+//     if (!isAuthor) {
+//       return res.status(400).json("you are not the author");
+//     }
+//     await postDoc.update({
+//       title,
+//       summary,
+//       content,
+//       cover: newPath ? newPath : postDoc.cover,
+//     });
+
+//     res.json(postDoc);
+//   });
+// });
+
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const { originalname, path } = req.file;
-  const parts = originalname.split(".");
-  const ext = parts[parts.length - 1];
-  const newPath = path + "." + ext;
+  const ext = path.extname(originalname);
+  const newPath = `${path}.${ext}`;
+
   fs.renameSync(path, newPath);
 
   const { token } = req.cookies;
@@ -189,9 +250,8 @@ app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
   let newPath = null;
   if (req.file) {
     const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
+    const ext = path.extname(originalname);
+    newPath = `${path}.${ext}`;
     fs.renameSync(path, newPath);
   }
 
