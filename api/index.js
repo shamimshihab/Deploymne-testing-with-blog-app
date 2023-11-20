d50274b70ba5a317da5e89ad42eaee6baeef35fc
Shamim Shihab <shamimshihab56@gmail.com>
2023-11-19 19:23:26 -0800

    update

--- api/index.js
+++ api/index.js
@@ -38,9 +38,24 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
-mongoose.connect(
-  "mongodb+srv://shamimshihab56:shihabblog56@cluster0.pgwf0fs.mongodb.net/?retryWrites=true&w=majority"
-);
+// mongoose.connect(
+//   "mongodb+srv://shamimshihab56:shihabblog56@cluster0.pgwf0fs.mongodb.net/?retryWrites=true&w=majority"
+// );
+
+mongoose
+  .connect(
+    "mongodb+srv://shamimshihab56:shihabblog56@cluster0.pgwf0fs.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
